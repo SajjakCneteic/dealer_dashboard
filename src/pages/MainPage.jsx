1947755388372cc694890f7cbfa7a7cdb59a4296
VishalGCnetric <vishal@cnetric.com>
2024-07-17 11:54:21 +0530

    mainpage

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -10,8 +10,8 @@ const MainPage = () => {
         <Navbar />
         <div className='flex-grow p-4 bg-gray-100'>
           {/* Main content goes here */}
-          <h1 className='text-2xl font-bold'>Welcome to the Main Page</h1>
-          <p>Here is the main content of the page.</p>
+          {/* <h1 className='text-2xl font-bold'>Welcome to the Main Page</h1>
+          <p>Here is the main content of the page.</p> */}
         </div>
       </div>
     </div>
